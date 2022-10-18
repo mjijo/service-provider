3ce81bab6d95cb32478105efcd1a2612e7a5024f
mjijo <96546469+mjijo@users.noreply.github.com>
2022-10-18 16:11:07 +0300

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonComponent } from './layout/common/common.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+
+    path: '',
+    component: CommonComponent,
+    children: [
+      {
+        path: 'dashboard',
+        children: [
+          { path: '', component: DashboardComponent }
+          // { path: 'all-movies', component: AllMoviesComponent },
+          // { path: 'coming-soon', component: ComingSoonComponent }
+
+        ]
+
+      },
+
+      { path: 'Dashboard', component: DashboardComponent },
+      { path: '', component: DashboardComponent},
+      { path: '**', redirectTo: 'movies' }
+
+    ]
+
+  },
+
+  // otherwise redirect to home
+
+  { path: '**', redirectTo: '' }
+
+];
+
+​
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CommonComponent } from './layout/common/common.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    DashboardComponent,
+    CommonComponent
   ],
   imports: [
     BrowserModule,
